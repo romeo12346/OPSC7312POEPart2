3a65d5e09a6df325be9a447525d315061ccb02ba
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-10-05 21:55:36 +0200

    A rough version of OPSC7312POEPart2 for now.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OPSC7312POEPart2"
+include(":app")
