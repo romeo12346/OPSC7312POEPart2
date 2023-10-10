4a9cdd5f8fea245dfcdf79c4cb94b43bd9e1e424
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-10-10 21:00:05 +0200

    Updated Fragments and added content (still in progress).

--- app/src/main/java/com/example/opsc7312poepart2/MainActivity.kt
+++ app/src/main/java/com/example/opsc7312poepart2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.opsc7312poepart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Adjusting buttons and values
+        val btnhotspots = findViewById(R.id.btnhotspots)
+        val btnnearby = findViewById(R.id.btnnearby)
+        val btnbirds = findViewById(R.id.btnbirds)
+        val btnrare = findViewById(R.id.btnrare)
+        val btnchecklists = findViewById(R.id.btnchecklists)
+        val btnsettings = findViewById(R.id.btnsettings)
     }
 }
