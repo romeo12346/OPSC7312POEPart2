794d981773a76bf81d68fb8ab05619c3c59114fd
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-10-11 16:48:54 +0200

    Added Variables and a Map.

--- app/src/main/java/com/example/opsc7312poepart2/Loginfrag.kt
+++ app/src/main/java/com/example/opsc7312poepart2/Loginfrag.kt
@@ -6,60 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Loginfrag.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Loginfrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        //Adjusting/Declaring buttons and values
-        val btnlogin: Button = fregment_loginfrag.findViewById(R.id.btnlogin)
-        val btnregisterpage: Button = fregment_loginfrag.findViewById(R.id.btnregisterpage)
-        val txtloginusername: TextView = fregment_loginfrag.(R.id.txtloginusername)
-        val txtloginpassword: TextView = fregment_loginfrag.(R.id.txtloginpassword)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val fragment_loginfrag = inflater.inflate(R.layout.fragment_loginfrag, container, false)
+        //Adjusting/Declaring buttons and values
+        val btnlogin: Button = fragment_loginfrag.findViewById(R.id.btnlogin)
+        val btnregisterpage: Button = fragment_loginfrag.findViewById(R.id.btnregisterpage)
+        val txtloginusername: TextView = fragment_loginfrag.findViewById(R.id.txtloginusername)
+        val txtloginpassword: TextView = fragment_loginfrag.findViewById(R.id.txtloginpassword)
+        //
         return inflater.inflate(R.layout.fragment_loginfrag, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Loginfrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Loginfrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        //return the variables
+        //val fragment_loginfrag = inflater.inflate(R.layout.fragment_loginfrag, container, false)
     }
 }
--- app/src/main/java/com/example/opsc7312poepart2/Settingsfrag.kt
+++ app/src/main/java/com/example/opsc7312poepart2/Settingsfrag.kt
@@ -7,59 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Settingsfrag.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Settingsfrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        //Adjusting/Declaring buttons and values
-        val btnkilometres: Button = fragment_settingsfrag.findViewById(R.id.btnkilometres)
-        val btnmiles: Button = fragment_settingsfrag.findViewById(R.id.btnmiles)
-        val txtmaxdistance: TextView = fragment_settingsfrag.findViewById(R.id.txtmaxdistance)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val fragment_settingsfrag = inflater.inflate(R.layout.fragment_settingsfrag, container, false)
+        //Adjusting/Declaring buttons and values
+        val btnkilometres: Button = fragment_settingsfrag.findViewById(R.id.btnkilometres)
+        val btnmiles: Button = fragment_settingsfrag.findViewById(R.id.btnmiles)
+        val txtmaxdistance: TextView = fragment_settingsfrag.findViewById(R.id.txtmaxdistance)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settingsfrag, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Settingsfrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Settingsfrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
