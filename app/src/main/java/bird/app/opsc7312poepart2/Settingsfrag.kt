83bf9b08660cefbc5f8aa4be7034d1a5d70ac791
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-10-15 15:56:04 +0200

    Replaced 'Settings' Buttons with Radio Buttons.

--- app/src/main/java/bird/app/opsc7312poepart2/Settingsfrag.kt
+++ app/src/main/java/bird/app/opsc7312poepart2/Settingsfrag.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -22,9 +22,13 @@ class Settingsfrag : Fragment() {
         val fragment_settingsfrag = inflater.inflate(R.layout.fragment_settingsfrag, container, false)
 
         //Adjusting/Declaring buttons and values
-        val btnkilometres: Button = fragment_settingsfrag.findViewById(R.id.btnkilometres)
-        val btnmiles: Button = fragment_settingsfrag.findViewById(R.id.btnmiles)
+        //val btnkilometres: Button = fragment_settingsfrag.findViewById(R.id.btnkilometres)
+        //val btnmiles: Button = fragment_settingsfrag.findViewById(R.id.btnmiles)
         val logout: Button = fragment_settingsfrag.findViewById(R.id.btnLogout)
+
+        //Changed previous buttons to radio buttons
+        val btnradiokilometres: RadioButton = fragment_settingsfrag.findViewById(R.id.btnradiokilometres)
+        val btnradiomiles: RadioButton = fragment_settingsfrag.findViewById(R.id.btnradiomiles)
 
         logout.setOnClickListener(){
             auth.signOut()
@@ -36,7 +40,7 @@ class Settingsfrag : Fragment() {
 
         //Proper Methods
         //Set App to kilometres -> 1 Km = 0,621371 Miles
-        btnkilometres.setOnClickListener(){
+        /*btnkilometres.setOnClickListener(){
             //val measurementKm = 1* 0.621371 //may work better as double value
             //Testing out method
             val measurement = 1
@@ -44,6 +48,22 @@ class Settingsfrag : Fragment() {
         }
         //Set App to Miles -> 1 Mile = 1,60934 Km
         btnmiles.setOnClickListener(){
+            //val measurementM = 1* 1.60934 //may work better as double value
+            //Testing out method
+            val measurement = 1
+            val measurementM = measurement* 0.621371
+        }*/
+
+        //New Radio buttons methods
+        //Set App to kilometres -> 1 Km = 0,621371 Miles
+        btnradiokilometres.setOnClickListener(){
+            //val measurementKm = 1* 0.621371 //may work better as double value
+            //Testing out method
+            val measurement = 1
+            val measurementKm = measurement* 0.621371
+        }
+        //Set App to Miles -> 1 Mile = 1,60934 Km
+        btnradiomiles.setOnClickListener(){
             //val measurementM = 1* 1.60934 //may work better as double value
             //Testing out method
             val measurement = 1
